9167a30acbee56cc0c1d487b5bf01af262357fdf
haoyan.zhang <haoyan.zhang@qunar.com>
2018-04-01 14:51:11 +0800

    6

--- src/main/java/com/healthmanager/util/CryptUtil.java
+++ src/main/java/com/healthmanager/util/CryptUtil.java
@@ -7,6 +7,8 @@ import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -59,11 +61,13 @@ public class CryptUtil {
 
     public static byte[] aesEncryptToBytes(String content, String encryptKey) {
         try {
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            kgen.init(128, new SecureRandom(encryptKey.getBytes()));
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            SecureRandom random=SecureRandom.getInstance("SHA1PRNG");
+            random.setSeed(encryptKey.getBytes());
+            keyGen.init(128, random);
 
             Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(kgen.generateKey().getEncoded(), "AES"));
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(keyGen.generateKey().getEncoded(), "AES"));
 
             return cipher.doFinal(content.getBytes("utf-8"));
         }
@@ -79,11 +83,13 @@ public class CryptUtil {
 
     public static String aesDecryptByBytes(byte[] encryptBytes, String decryptKey) {
         try {
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            kgen.init(128, new SecureRandom(decryptKey.getBytes()));
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            SecureRandom random=SecureRandom.getInstance("SHA1PRNG");
+            random.setSeed(decryptKey.getBytes());
+            keyGen.init(128, random);
 
             Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(kgen.generateKey().getEncoded(), "AES"));
+            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(keyGen.generateKey().getEncoded(), "AES"));
             byte[] decryptBytes = cipher.doFinal(encryptBytes);
 
             return new String(decryptBytes);
