0a2a057e61ea22446b3e8f4429f027e0d1e82bd2
haoyan.zhang <haoyan.zhang@qunar.com>
2018-04-02 18:00:59 +0800

    2

--- src/main/java/com/healthmanager/controller/BodyController.java
+++ src/main/java/com/healthmanager/controller/BodyController.java
@@ -4,6 +4,7 @@ import com.healthmanager.common.JsonResult;
 import com.healthmanager.model.BodyData;
 import com.healthmanager.param.BodyUpdateParam;
 import com.healthmanager.service.BodyService;
+import com.healthmanager.util.ValidUtil;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,14 @@ public class BodyController {
 
     @RequestMapping("/update")
     public JsonResult update(@RequestBody @Valid BodyUpdateParam bodyUpdateParam){
+        ValidUtil.checkLogin();
         bodyService.update(bodyUpdateParam);
         return JsonResult.success();
     }
 
     @RequestMapping("/query")
     public JsonResult query(){
+        ValidUtil.checkLogin();
         BodyData query = bodyService.query();
         return JsonResult.success(query);
     }
--- src/main/java/com/healthmanager/controller/HeartController.java
+++ src/main/java/com/healthmanager/controller/HeartController.java
@@ -4,6 +4,7 @@ import com.healthmanager.common.JsonResult;
 import com.healthmanager.model.HeartData;
 import com.healthmanager.param.HeartUpdateParam;
 import com.healthmanager.service.HeartService;
+import com.healthmanager.util.ValidUtil;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,14 @@ public class HeartController {
 
     @RequestMapping("/update")
     public JsonResult update(@RequestBody @Valid HeartUpdateParam heartUpdateParam){
+        ValidUtil.checkLogin();
         heartService.update(heartUpdateParam);
         return JsonResult.success();
     }
 
     @RequestMapping("/query")
     public JsonResult query(){
+        ValidUtil.checkLogin();
         HeartData query = heartService.query();
         return JsonResult.success(query);
     }
